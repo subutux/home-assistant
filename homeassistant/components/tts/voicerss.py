b208de99f69698db6add0b9089c48f8e6dd9b912
Stijn Van Campenhout <subutux@gmail.com>
2020-09-16 16:44:38 +0200

    Added all supported languages from voicetts
    See http://www.voicerss.org/api/  for the complete list of supported languages.

--- homeassistant/components/tts/voicerss.py
+++ homeassistant/components/tts/voicerss.py
@@ -33,10 +33,13 @@ ERROR_MSG = [
 ]
 
 SUPPORT_LANGUAGES = [
-    'ca-es', 'zh-cn', 'zh-hk', 'zh-tw', 'da-dk', 'nl-nl', 'en-au', 'en-ca',
-    'en-gb', 'en-in', 'en-us', 'fi-fi', 'fr-ca', 'fr-fr', 'de-de', 'it-it',
-    'ja-jp', 'ko-kr', 'nb-no', 'pl-pl', 'pt-br', 'pt-pt', 'ru-ru', 'es-mx',
-    'es-es', 'sv-se',
+    'ar-eg', 'ar-sa', 'bg-bg', 'ca-es', 'zh-cn', 'zh-hk', 'zh-tw', 'hr-hr', 
+    'cs-cz', 'da-dk', 'nl-be', 'nl-nl', 'en-au', 'en-ca', 'en-gb', 'en-in', 
+    'en-ie', 'en-us', 'fi-fi', 'fr-ca', 'fr-fr', 'fr-ch', 'de-at', 'de-de', 
+    'de-ch', 'el-gr', 'he-il', 'hi-in', 'hu-hu', 'id-id', 'it-it', 'ja-jp', 
+    'ko-kr', 'ms-my', 'nb-no', 'pl-pl', 'pt-br', 'pt-pt', 'ro-ro', 'ru-ru', 
+    'sk-sk', 'sl-si', 'es-mx', 'es-es', 'sv-se', 'ta-in', 'th-th', 'tr-tr', 
+    'vi-vn',    
 ]
 
 SUPPORT_CODECS = [
